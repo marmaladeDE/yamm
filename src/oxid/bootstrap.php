acde4a9899fea63dce30ae0ef1944ff023b83d7b
Stefan Krenz <krenz@marmalade.de>
2018-11-07 11:52:13 +0100

    Added check if composer autoloader exists.

--- src/oxid/bootstrap.php
+++ src/oxid/bootstrap.php
@@ -26,7 +26,9 @@ if(!function_exists('oxNew'))
     require_once __DIR__ . '/../../../../../bootstrap.php';
 }
 
-require_once __DIR__ . '/../../vendor/autoload.php';
+if (file_exists(__DIR__ . '/../../vendor/autoload.php')) {
+    require_once __DIR__ . '/../../vendor/autoload.php';
+}
 
 $shopRoot = OX_BASE_PATH;
 $moduleDirectory = "{$shopRoot}modules";
